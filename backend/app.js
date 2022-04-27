a620765e9aa94ee0d1dffc4075d08f89b6b5b613
thesafatem <thesafatem@gmail.com>
2022-04-27 16:58:54 +0600

    cors

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(cors());
-
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
