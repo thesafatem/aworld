c0f7cafd35b1a024ad08657ce24dcd56b045ae79
thesafatem <thesafatem@gmail.com>
2022-04-27 17:23:39 +0600

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 // app.use(function(req, res, next) {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
