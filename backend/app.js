57e0c78987617e7f6d57135bf2b2bf7398d1803f
thesafatem <thesafatem@gmail.com>
2022-04-27 16:43:53 +0600

    cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/auth', require('./routers/auth.js'));
 app.use('/comments', require('./routers/comment.js'));
 app.use('/posts', require('./routers/post.js'));
