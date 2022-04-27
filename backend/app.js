097d05a3ea479af55ddc18790af01e23df55234a
thesafatem <thesafatem@gmail.com>
2022-04-27 16:55:22 +0600

    cors

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,14 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/auth', require('./routers/auth.js'));
 app.use('/comments', require('./routers/comment.js'));
 app.use('/posts', require('./routers/post.js'));
